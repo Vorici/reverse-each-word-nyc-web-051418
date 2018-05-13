096ccae3da775940bfe94d91e4e12855d4e59938
Ledio Bode <l.bode515@gmail.com>
2018-05-13 20:40:24 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 
 
 def reverse_each_word(string)
-  reversed_string = string.split.collect do |word|
-    word.reverse
+  string.split.collect do |word|
+    word.reverse.join(" ")
   end
-  reversed_string.join(" ")
 end
